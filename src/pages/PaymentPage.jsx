c4e26bec7ebeae47542215edf29f57089943c1bd
nikhilmathur1215-create <nikhil.mathur1215@gmail.com>
2026-06-01 20:52:05 +0530

    Update PaymentPage.jsx

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { QRCodeSVG } from 'qrcode.react';
+import QRCode from 'react-qr-code';
 import api from '../services/api';
 
 const UPI_ID = '8800244169@upi';
